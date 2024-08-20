268609ad635df85f41fdbf90cb7494e61bc72b30
Harsh Mahadev Duche <ducheharsh@gmail.com>
2024-08-20 15:41:36 +0530

    modificatons for deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
