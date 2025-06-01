1f306a4bc319ffe9e7811db966bc882b0d034ca3
Harsh-Duche <ducheharsh@gmail.com>
2025-06-01 18:25:52 +0530

    chore: added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,36 @@ import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Harsh Duche | Portfolio"
+  title: "Harsh Duche | Portfolio",
+  icons: {
+    icon: "/boy.png"
+  },
+  description: "Harsh Duche's Portfolio",
+  keywords: ["Harsh Duche", "Portfolio", "Harsh", "Duche"],
+  authors: [{ name: "Harsh Duche", url: "https://harshduche.com" }],
+  creator: "Harsh Duche",
+  publisher: "Harsh Duche",
+  openGraph: {
+    title: "Harsh Duche | Portfolio",
+    description: "Harsh Duche's Portfolio",
+    url: "https://harshduche.com",
+    siteName: "Harsh Duche",
+    images: [
+      {
+        url: "dev.png",
+        width: 800,
+        height: 600,
+        alt: "Harsh Duche",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Harsh Duche | Portfolio",
+    description: "Harsh Duche's Portfolio",
+    images: ["dev.png"],
+  },
+  metadataBase: new URL("https://harshduche.com"),
 };
 
 export default function RootLayout({
@@ -18,8 +47,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <SpeedInsights/>
-      <Analytics/>
+      <SpeedInsights />
+      <Analytics />
       <body className={inter.className}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
