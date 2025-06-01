883a79159b3b18fc63102930e40b94de3cff83af
Harsh-Duche <ducheharsh@gmail.com>
2025-06-01 18:33:30 +0530

    fix: metadata image width ratio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "dev.png",
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 630,
         alt: "Harsh Duche",
       },
     ],
